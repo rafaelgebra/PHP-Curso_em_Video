d5431b2a806ab51f7cdb3a314f05dd2d92ab0f2e
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-05-29 16:57:49 -0300

    d001_PHP_update

--- desafios/d001/cad.php
+++ desafios/d001/cad.php
@@ -13,17 +13,19 @@
     <main>
         <h1>Resultado Final</h1>
         <?php 
-        $num = $_GET["num"] ?? "0";
+        $num = $_GET["num"] ?? 0 ;
         $num_antecessor = $num - 1;
         $num_sucessor = $num + 1;
         print("O número escolhido foi <strong>$num</strong>.") ;
         // também pode ser frito da segunte forma sem as variáveis.
         // print("O seu seu <em>antecessor</em> é $num_antecessor" . ($num - 1) ."\n")
-        print("O seu <em>antecessor</em> é $num_antecessor ") ;
+        print( "<br>O seu <em>antecessor</em> é $num_antecessor ") ;
          // print("O seu <em>sucessor</em> é $num_sucessor" .($num + 1) ."\n")
-        print("O seu <em>sucessor</em> é $num_sucessor ")          
+        print(" <br>O seu <em>sucessor</em> é $num_sucessor ")          
         ?>
-        <p><a href="javascript:history.go(-1)"> u\{2B05} <-Voltar</a></p>
+        <button onclick="javascript:window.location.href='index.html'">&#x2B05 Voltar</button>
+        <!--outra opção de fazer o botão de VOLTAR é:
+        <button onclick="javascript:history.go(-1)">&#x2B05; Valtar</button> -->
     </main>
 
 
