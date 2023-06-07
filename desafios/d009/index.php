d3c0ec194b4a6daae83d7e1a15b379efc8f2ef25
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-06-07 13:55:03 -0300

    Update_d009.PHP

--- desafios/d009/index.php
+++ desafios/d009/index.php
@@ -9,22 +9,22 @@
 </head>
 <body>
     <?php 
-    $valor1 = $_GET['valor1']??0;
-    $valor2 = $_GET['valor2']??0;
-    $peso1 = $_GET['peso1']??0;
-    $peso2 = $_GET['peso2']??0;
+    $valor1 = $_GET['valor1']?? '';
+    $valor2 = $_GET['valor2']?? '';
+    $peso1 = $_GET['peso1']?? '';
+    $peso2 = $_GET['peso2']?? '';
     ?>
     <main>
         <h1>Médias Aritméticas</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="valor1">1° Valor</label>
-            <input type="number" name="valor1" id="valor1" value="<?=$valor1?>">
+            <input type="number" name="valor1" id="valor1" required  value="<?=$valor1?>">
             <label for="peso1">1° Peso</label>
-            <input type="number" name="peso1" id="peso1" value="<?=$peso1?>">
+            <input type="number" name="peso1" id="peso1" required value="<?=$peso1?>">
             <label for="valor2">2° Valor</label>
-            <input type="number" name="valor2" id="valor2" value="<?=$valor2?>">
+            <input type="number" name="valor2" id="valor2" required value="<?=$valor2?>">
             <label for="peso2">2° Peso</label>
-            <input type="number" name="peso2" id="peso2" value="<?=$peso2?>">
+            <input type="number" name="peso2" id="peso2" required value="<?=$peso2?>">
             <input type="submit" value="Calucular Médidas"> 
     
         </form>
@@ -33,11 +33,8 @@
     <section>
             <h2>Cálculo das Médidas</h2>
             <?php 
-            $valorTotal = $valor1 + $valor2;
-            $mediaSimples = $valorTotal / 2;
-            $baseMediaPonderada = ($peso1*$valor1)+($peso2*$valor2); 
-            $basePeso = $peso1+$peso2;
-            $mediaPonderada = $baseMediaPonderada / $basePeso;
+            $mediaSimples = ($valor1 + $valor2) / 2;
+            $mediaPonderada = (($peso1*$valor1) + ($peso2*$valor2)/($peso1+$peso2)); 
 
 
             print"<p>Analisando os valores $valor1 e $valor2: </p> <br>";
