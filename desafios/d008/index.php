3d3a328197c41d166b85bf09077dd05b15bcd80c
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-06-07 12:27:27 -0300

    Update_d008.PHP

--- desafios/d008/index.php
+++ desafios/d008/index.php
@@ -10,25 +10,27 @@
 <body>
     <?php 
     $numero = $_GET['numero']??0;
-    ?>
-    <main>
-    <h1>Informe um número</h1>
-    <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-        <label for="Numero">Número</label>
-        <input type="number" name="numero" id="numero">
-        <input type="submit" value="Calcular">
 
-    </form>
+    ?>
+
+    <main>
+        <h1>Informe um número</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="Numero">Número</label>
+            <input type="number" name="numero" id="numero" min="0"  value="<?=$numero?>">
+            <input type="submit" value="Calcular">
+
+        </form>
 
     </main>    
         <section>
             <h2>Resultado final</h2>
         <?php
-            $raizQuadrada = $numero ** (1/2);
-            $raizCubica = $numero ** (1/3);
+            $raizQuadrada = sqrt($numero);//escrito em forma de raiz quadrada. lembrando que também da para escrever de forma de potência, $raizquadrada = $numero ** (1/2);
+            $raizCubica = $numero ** (1/3);//Escrito em forma de potência. lembrando que ainda não tem formula para raiz cubixa
 
 
-            print"<p>Analizando o número " .number_format($numero,2,",",".")." temos:</p>";
+            print"<p>Analizando o <strong>número " .number_format($numero,0,",",".")." </strong>temos:</p>";
             print"<ul><li>A dua raiz quadrada é <strong>".number_format($raizQuadrada, 3, ",", ".")."</strong></li> <br>
             <li>A dua raiz cúbica é<strong> " .number_format($raizCubica, 3,",",".")."</strong></li></ul>";
         
