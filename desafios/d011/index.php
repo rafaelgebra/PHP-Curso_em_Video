22945c3d584917bb9e25b53378b51d119d862d96
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-06-08 16:09:58 -0300

    update_d011_FIM.PHP

--- desafios/d011/index.php
+++ desafios/d011/index.php
@@ -11,30 +11,30 @@
     <?php 
     $preco = $_GET['preco']??0;
     $percentual = $_GET['percentual']??0;
-    $aumento = ($preco / 100) * $percentual;
+    $aumento = ($preco * $percentual) / 100 ;
     $precoFinal = $preco +$aumento;
     ?>
-    
     <main>
         <h1>Reajustador de Preços</h1>
             <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
                 <label for="preco">Preço do Produto(R$)</label>
-                <input type="number" name="preco" id="preco" value="<?=$preco?>" step="0.01">
-                <label for="percentual">Qual será o precentual de reajuste?</label>
-                <input type="number" name="percentual" id="percentual" value="<?=$percentual?>">
+                <input type="number" name="preco" id="preco" value="<?=$preco?>" min="0.10" step="0.01">
+                <label for="percentual">Qual será o precentual de reajuste? (<strong><span id="p">? </span>%</strong>)</label>
+                <input type="range" name="percentual" id="percentual" min="0" max="100" step="1"  value="<?=$percentual?>" oninput="mudaValor()">
                 <input type="submit" value="Reajustar">
-
             </form>
-
     </main>
     <section>
         <h2>Resultado do Reajuste</h2>
         <?php 
-        print"O produto que custava R\$".number_format($preco,2,",",".")." com <strong> $percentual% de aumento</strong> vai passar a custar <strong>R\$".number_format($precoFinal,2,",",".")."</strong> a partir de agora.";
-        
+        print"<p>O produto que custava R$".number_format($preco,2,",",".")." com <strong> $percentual% de aumento</strong> vai passar a custar <strong>R$".number_format($precoFinal,2,",",".")."</strong> a partir de agora.</p>";
         ?>
-
-
     </section>
+    <script>
+        mudaValor()
+        function mudaValor() {
+            p.innerText = percentual.value
+        }
+    </script>
 </body>
 </html>
