92561bb5c55dd19d959941e18904c64e65b83ec3
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-06-08 16:24:21 -0300

    Update_d012_FIM.PHP

--- desafios/d012/index.php
+++ desafios/d012/index.php
@@ -9,43 +9,34 @@
 </head>
 <body>
     <?php 
-        $segundos = $_GET['segundos'];
+        $segundos = $_GET['segundos']??0;
+        //Total de semanas
         $semanas = (int) ($segundos / 604800);
         $restoSegundos = $segundos % 604800;
+        //Total de dias
         $dias = (int) ($restoSegundos/ 86400);
         $restoSegundos = $segundos % 86400;
+        //Total de horas
         $horas = (int) ($restoSegundos / 3600);
         $restoSegundos =  $segundos % 3600;
+        //Total de minutos
         $minutos = (int) ($restoSegundos / 60);
+        //Total de segundos restantes
         $restoSegundos = $restoSegundos % 60;
-    
     ?>
-
-
     <main>
         <h1>Calculadora de tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="segundos">Qual é o tempo de segundos</label>
-            <input type="number" name="segundos" id="segundos" value="<?=$segundos?>">
+            <input type="number" name="segundos" id="segundos" min="0" step="1" required value="<?=$segundos?>">
             <input type="submit" value="Calcular">
-
         </form>
-        
     </main>
-
     <section>
         <h2>Totalizando tudo</h2>
         <?php 
-        
             print"Analisando o valor que você digitou, <strong>".number_format($segundos,0,".",".")." segundos</strong> equivalem a um total de: <ul><li>$semanas semanas</li> <li>$dias dias</li><li>$horas horas</li><li>$minutos minutos</li><li>$restoSegundos segundos</li></ul>";
-        
-
-            print"$intDia";
-        ?>
-
+       ?>
     </section>
-
-
-
 </body>
 </html>
