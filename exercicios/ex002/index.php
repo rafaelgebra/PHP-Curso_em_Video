1892244d25a83d58d86748e870cdaee1ffe5655a
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2024-03-01 17:25:28 -0300

    Update_theory

--- exercicios/ex002/index.php
+++ exercicios/ex002/index.php
@@ -9,8 +9,14 @@
 <body>
     <h1>Exemplo PHP</h1>
     <?php // Só funciona se por <?php senão não funciona
-        date_default_timezone_set("America/Sao_Paulo");
-        echo "Hoje é dia " . date("d/M/Y");
-        echo " e a hora atual é " . date("G:i:s");    ?>
+        date_default_timezone_set("America/Sao_Paulo"); // timezone BRASIL
+        echo "Hoje é dia " . date("d/M/Y") . " - Com interpretação de conteúdo  com aspas duplas \"\"<br>";
+   
+        echo 'Hoje é dia ' . date('d/M/Y') . " - Sem interpretação de conteúdo com aspas simples ''<br>";
+        echo "E a hora atual é " . date("G:i:s") . " - Com interpretação do conteúdo com aspas duplas \"\"<br> "; // Para mostrar a hora é: G (maiuscolo) é hora, i (minisculo) é minutos, s (minisculo) é o segundo.
+        echo 'E a hora atual é ' . date('G:i:s') . " - Sem interpretação do conteúdo com aspas simples ''<br>";
+
+        ?>
+
 </body>
 </html>
